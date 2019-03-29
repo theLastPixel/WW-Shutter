4092e3ad44aa592bf30b98ca83a71a485cc9f2e6
Roberto E. M. Austin <0315108515@miutt.edu.mx>
2019-03-29 16:06:39 -0700

    Se eliminan los primero archivos de prueba, se termina de configurar y conectar expresss.js, los archivos sass estan listos para trabajarse con cada uno de los archivos.

---
+++ src/index.js
@@ -0,0 +1,47 @@
+//Requires
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path');
+
+//Initializations
+const app = express();
+app.use(expressLayout);
+
+//Settings
+app.set('port', process.env.PORT || 5000);
+app.set('views', path.join(__dirname) + '/views');
+app.set('view engine', 'ejs');
+
+//MiddleWares
+
+//Global Variables
+
+//Routes
+app.get('/catalogo', (req, res) => {
+    res.render('catalogo');
+});
+
+app.get('/conocenos', (req, res) => {
+    res.render('conocenos');
+});
+
+app.get('/contactanos', (req, res) => {
+    res.render('contactanos');
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/tiposShutter', (req, res) => {
+    res.render('typeOfShutter');
+});
+
+//Static Files
+app.use(express.static(path.join(__dirname, 'styles')));
+app.use(express.static(path.join(__dirname, 'styles')));
+
+//Server is listening
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
