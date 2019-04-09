a982083c1b8c28115695008d3d05cff6b83dea4d
Roberto E. M. Austin <0315108515@miutt.edu.mx>
2019-04-09 15:08:43 -0700

    prueba de tipos de shutter

--- src/index.js
+++ src/index.js
@@ -47,9 +47,10 @@ app.get('/shutter/Estandar', (req, res) => {
 app.get('/shutter/PuertaFrancesa', (req, res) => {
     res.render('ShutterPuertaFrancesa');
 });
-app.get('/shutter/Arco', (req, res) => {
-    res.render('ShutterArco');
-});
+
+// app.get('/shutter/Arco', (req, res) => {
+//     res.render('ShutterArco');
+// });
 
 //Static Files
 app.use(express.static(path.join(__dirname + '/public')));
