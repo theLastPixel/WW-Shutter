ca97a737d744cd36a218d6e778f70dc8bc1813b6
Roberto E. M. Austin <0315108515@miutt.edu.mx>
2019-04-23 12:40:27 -0700

    Se coloca icono en la pestaña

--- src/index.js
+++ src/index.js
@@ -118,6 +118,109 @@ app.get('/galeria-14', (req, res) => {
     res.render('galeria-14');
 });
 
+/*--------------ENGLISH----------------*/
+
+//Routes
+
+app.get('/contactanos-English', (req, res) => {
+    res.render('English/contactanosUSA');
+});
+
+app.get('/-English', (req, res) => {
+    res.render('English/indexUSA');
+});
+
+/*TIPOS DE SHUTTER*/
+app.get('/shutter/Corrediza-English', (req, res) => {
+    res.render('English/shutterCorredizaUSA');
+});
+app.get('/shutter/Estandar-English', (req, res) => {
+    res.render('English/shutterEstandarUSA');
+});
+app.get('/shutter/PuertaFrancesa-English', (req, res) => {
+    res.render('English/shutterPuertaFrancesaUSA');
+});
+app.get('/shutter/Arco-English', (req, res) => {
+    res.render('English/shutterArcoUSA');
+});
+
+/*CONSEJOS*/
+app.get('/consejos/consejo-1-English', (req, res) => {
+    res.render('English/consejoUnoUSA');
+});
+
+app.get('/consejos/consejo-2-English', (req, res) => {
+    res.render('English/consejoDos')
+});
+
+/*Nosotros*/
+app.get('/nosotros/conocenos-English', (req, res) => {
+    res.render('English/conocenosUSA');
+});
+
+app.get('/nosotros/nuestroEquipo-English', (req, res) => {
+    res.render('English/nuestroEquipo')
+});
+
+/*Galeria*/
+app.get('/galeria-1-English', (req, res) => {
+    res.render('English/galeria-1USA');
+});
+
+app.get('/galeria-2-English', (req, res) => {
+    res.render('English/galeria-2USA');
+});
+
+app.get('/galeria-3-English', (req, res) => {
+    res.render('English/galeria-3USA');
+});
+
+app.get('/galeria-4-English', (req, res) => {
+    res.render('English/galeria-4USA');
+});
+
+app.get('/galeria-5-English', (req, res) => {
+    res.render('English/galeria-5USA');
+});
+
+app.get('/galeria-6-English', (req, res) => {
+    res.render('English/galeria-6USA');
+});
+
+app.get('/galeria-7-English', (req, res) => {
+    res.render('English/galeria-7USA');
+});
+
+app.get('/galeria-8-English', (req, res) => {
+    res.render('English/galeria-8USA');
+});
+
+app.get('/galeria-9-English', (req, res) => {
+    res.render('English/galeria-9USA');
+});
+
+app.get('/galeria-10-English', (req, res) => {
+    res.render('English/galeria-10USA');
+});
+
+app.get('/galeria-11-English', (req, res) => {
+    res.render('English/galeria-11USA');
+});
+
+app.get('/galeria-12-English', (req, res) => {
+    res.render('English/galeria-12USA');
+});
+
+app.get('/galeria-13-English', (req, res) => {
+    res.render('English/galeria-13USA');
+});
+
+app.get('/galeria-14-English', (req, res) => {
+    res.render('English/galeria-14USA');
+});
+
+
+
 //Static Files
 app.use(express.static(path.join(__dirname + '/public')));
 
