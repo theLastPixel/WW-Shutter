2f1958bc3845954cbac1bbaf8c8516e7aed1792b
Roberto E. M. Austin <0315108515@miutt.edu.mx>
2019-04-26 19:31:54 -0700

    Prueba 2

--- src/index.js
+++ src/index.js
@@ -17,11 +17,10 @@ app.set('views', path.join(__dirname) + '/views');
 app.set('view engine', 'ejs');
 
 //Server
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('Hello World\n');
-//   });
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+   });
 
   //Server is listening
 // app.listen(app.get('port'), () => {
